ad060360a22eedbab21449fad4de10fef1a0fee4
kam <adouardcamille@hotmail.com>
2018-02-06 12:55:56 +0100

    Add Cleanup Script and Demo Script

---
+++ docker-demo.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+#Script automation demonstration of Docker
+
+clear
+
+echo "#################################################################"
+echo "#                                                               #"
+echo "#    Welcome in the automated demonstration script of  Docker   #"
+echo "#                                                               #"
+echo "#################################################################"
+echo "#"
+echo "Press 1 if you want to see the creation of a container"
+echo "#"
+echo "Press 2 if you want to setup a development environment"
+echo "#"
+echo "Press 3 if you want to setup a production environement"
+echo "#"
+echo "Press 4 if you want to setup a continuous integration process environment"
+echo "#"
+echo "Press 5 to quit this script"
+read menuS
+
+case "$menuS" in
+
+"1")
+echo "You have picked to see the creation of a container !: "
+echo "docker container run --name nginx --rm -d -p 8088:80 nginx:latest"
+docker container run --name nginx --rm -d -p 8088:80 nginx:latest > dump.txt
+cat dump.txt
+docker container inspect nginx | grep IPAddress  > dump.txt
+echo "This command has run a container of a nginx server you can reach it on:"
+echo "127.0.0.1:8088" 
+echo "or the container ip address below, at the port : 80"
+cat dump.txt
+sleep 3
+echo "Press any key to clean the container and the associated" 
+echo "volumes and go back to the main menu"
+read goback
+    case "$goback" in
+    *)
+    docker container stop nginx
+    clear
+    exec /home/kam/Courses/Docker/Scripts/docker-demo.sh;
+    ;;
+    esac
+;;
+
+"2")
+echo "You have picked to see the creation of a development environment"
+echo "docker container run --name node-envir --rm -d -p 3000:3000 nodejs-example-envir"
+docker container run --name node-envir --rm -d -p 3000:3000 nodejs-example-envir > dump.txt
+cat dump.txt
+docker container inspect node-envir | grep IPAddress > dump.txt
+echo "This command has run a container with an installation of nodeJs and the nodeJs framework Express:"
+echo "127.0.0.1:3000" 
+echo "or the container ip address below at the port : 3000"
+cat dump.txt
+sleep 3
+echo "Press any key to clean the container and the associated" 
+echo "volumes and go back to the main menu"
+read goback
+    case "$goback" in
+    *)
+    docker container stop node-envir
+    clear
+    exec /home/kam/Courses/Docker/Scripts/docker-demo.sh;
+    ;;
+    esac
+;;
+
+"3")
+echo "You have picked to see the creation of a production environment"
+echo "docker container run --name tomcat-prod --rm -d -p 8090:8080 tomcat-custom-deploy"
+docker container run --name tomcat-prod --rm -d -p 8090:8080 tomcat-custom-deploy > dump.txt
+cat dump.txt
+docker container inspect tomcat-prod | grep IPAddress > dump.txt
+echo "This command has run a container of a tomcat server which has deployed a war file, you can reach it on:"
+echo "127.0.0.1:8088/sample" 
+echo "your login to access tomcat manager is manager/root" 
+echo "or the container ip address below at the port : 8080/sample"
+cat dump.txt
+sleep 3
+echo "Press any key to clean the container and the associated" 
+echo "volumes and go back to the main menu"
+read goback
+    case "$goback" in
+    *)
+    docker container stop tomcat-prod
+    clear
+    exec /home/kam/Courses/Docker/Scripts/docker-demo.sh;
+    ;;
+    esac
+;;
+
+"4")
+echo "You have pick to see the creation of a continous integration process environment"
+echo "docker-compose up -d"
+docker-compose up -d > dump.txt
+cat dump.txt
+echo "Tomcat container, ip address below and port : 8080"
+docker container inspect scripts_tomcat_1 | grep IPAddress > dump.txt
+cat dump.txt
+echo "Sonarqube container, ip address below and port : 9000"
+docker container inspect scripts_sonarqube_1 | grep IPAddress > dump.txt
+cat dump.txt
+echo "GitLab container, ip address below and port : 80"
+docker container inspect scripts_gitlab_1 | grep IPAddress > dump.txt
+cat dump.txt
+echo "Jenkins container, ip address below and port : 8080"
+docker container inspect scripts_jenkins_1 | grep IPAddress > dump.txt
+cat dump.txt
+sleep 3
+echo "Press any key to clean the containers and the associated" 
+echo "volumes and go back to the main menu"
+read goback
+    case "$goback" in
+    *)
+    docker-compose down
+    clear
+    exec /home/kam/Courses/Docker/Scripts/docker-demo.sh;
+    ;;
+    esac
+;;
+
+"5")
+echo "You are leaving this script, goodbye & see you soon"
+rm dump.txt
+sleep 2
+clear
+;;
+
+*)
+echo "This number is not a viable option"   
+sleep 2
+exec /home/kam/Courses/Docker/Scripts/docker-demo.sh;
+;;
+esac
---
+++ nuke-docker.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+#Script automation for Docker cleanup
+
+clear
+
+echo "#############################################################"
+echo "#                                                           #"
+echo "#   Welcome in the automated cleanup script for Docker      #"
+echo "#                                                           #"
+echo "#############################################################"
+echo "#"
+echo "Press 1 if you want to delete all the current containers"
+echo "#"
+echo "Press 2 if you want to delete all the current volumes"
+echo "#"
+echo "Press 3 if you want to delete all the current images"
+echo "#"
+echo "Press 4 if you want to delete all the current services"
+echo "#"
+echo "Press 5 to quit this script"
+read menuS
+
+case "$menuS" in
+
+"1")
+echo "Here are the containers currently on your machine: "
+docker ps -a > dump.txt
+cat dump.txt
+echo "This will delete all the containers running or not, please confirm Y/N"
+read confirm
+if [ $confirm = Y ]
+then
+    echo "All containers are being suppressed ..."
+    docker rm $(docker ps -a -q) -f 
+    echo "All container have been deleted, you are goind back to the main menu"
+    sleep 3
+else
+    echo "The containers will be safe, you are going back to the main menu ..."
+    sleep 3
+fi
+    clear
+    exec /home/kam/Courses/Docker/nuke-docker.sh;
+;;
+
+"2")
+echo "Here are the volumes currently on your machine"
+docker volume ls > dump.txt
+cat dump.txt
+echo "This will delete all the volumes on your machine, please confirm Y/N"
+read confirm
+if [ $confirm = Y ]
+then
+    echo "All volumes are being suppressed ..."
+    docker volume rm $(docker volume ls -q) -f 
+    echo "All volumes have been deleted, you are goind back to the main menu"
+    sleep 3
+else
+    echo "The volumes will be safe, you are going back to the main menu ..."
+    sleep 3
+fi
+    clear
+    exec /home/kam/Courses/Docker/nuke-docker.sh;
+;;
+
+"3")
+echo "Here are the images currently on your machine"
+docker image ls | sort > dump.txt
+cat dump.txt
+echo "This will delete all the images on your machine, please confirm Y/N"
+read confirm
+if [ $confirm = Y ]
+then
+    echo "All images are being suppressed ..."
+    #docker rmi $(docker image -q) -f 
+    echo "All images have been deleted, you are goind back to the main menu"
+    sleep 3
+else
+    echo "The images will be safe, you are going back to the main menu ..."
+    sleep 3
+fi
+    clear
+    exec /home/kam/Courses/Docker/nuke-docker.sh;
+;;
+
+"4")
+echo "Here are the services currently on your machine"
+docker service ls > dump.txt
+cat dump.txt
+echo "This will delete all the services on your machine, please confirm Y/N"
+read confirm
+if [ $confirm = Y ]
+then
+    echo "All services are being suppressed ..."
+    docker service rm $(docker service ls -q) 
+    echo "All services have been deleted, you are goind back to the main menu"
+    sleep 3
+else
+    echo "The services will be safe, you are going back to the main menu ..."
+    sleep 3
+fi
+    clear
+    exec /home/kam/Courses/Docker/nuke-docker.sh;
+;;
+
+"5")
+echo "You are leaving this script, goodbye & see you soon"
+rm dump.txt
+sleep 2
+clear
+;;
+
+*)
+    echo "This number is not a viable option"
+    sleep 2 
+	exec /home/kam/Courses/Docker/nuke-docker.sh;
+;;
+esac
